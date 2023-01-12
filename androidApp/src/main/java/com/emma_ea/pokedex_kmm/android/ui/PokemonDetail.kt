a1e30d2d8bb5ddc6ba63ee423a95ee76664702d9
emmanuel awuah <emmanuelawuah.dev@gmail.com>
2023-01-12 06:16:14 +0000

    remove shadow from topbar

--- androidApp/src/main/java/com/emma_ea/pokedex_kmm/android/ui/PokemonDetail.kt
+++ androidApp/src/main/java/com/emma_ea/pokedex_kmm/android/ui/PokemonDetail.kt
@@ -1,6 +1,7 @@
 package com.emma_ea.pokedex_kmm.android.ui
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.rememberScrollableState
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
@@ -26,37 +27,55 @@ fun PokemonDetail() {
     val scrollState = rememberScrollState()
     Scaffold(
         modifier = Modifier.fillMaxSize(),
+        backgroundColor = Color(greenSeaColor),
         topBar = { TopBar() }
     ) {
-        Column(modifier = Modifier.padding(horizontal = 30.dp)) {
-            Row(
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier.fillMaxWidth()
+        Box {
+            Column(
+                modifier = Modifier.padding(horizontal = 30.dp)
             ) {
-                Column {
-                    Text(
-                        text = "Bulbasaur",
-                        fontSize = 20.sp,
-                        fontWeight = FontWeight.Bold,
-                    )
+                Row(
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Column {
+                        Text(
+                            text = "Bulbasaur",
+                            fontSize = 30.sp,
+                            color = Color.White,
+                            fontWeight = FontWeight.Bold,
+                        )
 
-                    Row() {
-                        Text(text = "Grass")
-                        Spacer(modifier = Modifier.width(8.dp))
-                        Text(text = "Poison")
+                        Row() {
+                            Text(text = "Grass", color = Color.White,)
+                            Spacer(modifier = Modifier.width(8.dp))
+                            Text(text = "Poison", color = Color.White,)
+                        }
+
                     }
-
+                    Text(text = "#001", color = Color.White,)
                 }
-                Text(text = "#001")
+
+                Image(
+                    painter = painterResource(id = R.drawable.pngegg),
+                    contentDescription = "balbasaur image"
+                )
+
             }
 
-            Image(
-                painter = painterResource(id = R.drawable.pngegg),
-                contentDescription = "balbasaur image"
-            )
-
+            Card(
+                modifier = Modifier.fillMaxWidth()
+                    .align(Alignment.BottomCenter)
+            ) {
+                Column() {
+                    Text(text = "Grass")
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(text = "Poison")
+                }
+            }
         }
+
     }
 }
 
@@ -64,16 +83,17 @@ fun PokemonDetail() {
 fun TopBar() {
     TopAppBar(
         navigationIcon = {
-            Icon(Icons.Filled.ArrowBack, contentDescription = "back arrow")
+            Icon(Icons.Filled.ArrowBack, tint = Color.White, contentDescription = "back arrow")
         },
         title = {},
         actions = {
             IconButton(onClick = {}) {
-                Icon(Icons.Filled.FavoriteBorder, contentDescription = "favorite icon")
+                Icon(Icons.Filled.FavoriteBorder, tint = Color.White, contentDescription = "favorite icon")
             }
         },
         backgroundColor = Color.Transparent,
-        modifier = Modifier.padding(horizontal = 20.dp, vertical = 30.dp)
+        modifier = Modifier.padding(horizontal = 20.dp, vertical = 30.dp),
+        elevation = 0.dp
     )
 }
 
